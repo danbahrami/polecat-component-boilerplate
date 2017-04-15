e65453c1897e0cbe06cbe59c50c4122d2179bedd
danbahrami <dan.bahrami@googlemail.com>
2017-04-15 10:14:55 +0100

    Extract file list and print utility modules

--- rcc.js
+++ rcc.js
@@ -2,6 +2,8 @@
 var program = require('commander');
 var paramCase = require('param-case');
 var pascalCase = require('pascal-case');
+var getFileList = require('./utilities/get-file-list');
+var printOutput = require('./utilities/print-output');
 
 program
   .version('0.0.1')
@@ -15,19 +17,6 @@ var componentName = pascalCase(program.args[1]);
 var fileName = paramCase(program.args[1]);
 var connected = program.connected;
 var styled = program.styled;
+var files = getFileList(fileName, connected, styled);
 
-console.log('Template created for ' + componentName + ':');
-console.log(path);
-console.log('├── ...');
-console.log('├── ' + fileName);
-console.log('│   ├── index.js');
-console.log('│   ├── ' + fileName + '-component.js');
-console.log('│   └── ' + fileName + '-component-test.js')
-if (connected) {
-	console.log('│   └── ' + fileName + '-connector.js');
-	console.log('│   └── ' + fileName + '-connector-test.js');
-}
-if (styled) {
-	console.log('│   └── ' + fileName + '-style.scss');
-}
-console.log('└── ...');
+printOutput(path, componentName, files);
---
+++ utilities/print-output.js
@@ -0,0 +1,17 @@
+var paramCase = require('param-case');
+
+module.exports = function (path, componentName, files) {
+	var fileName = paramCase(componentName);
+
+	console.log('Template created for ' + componentName + ':');
+	console.log(path);
+	console.log('├── ...');
+	console.log('├── ' + fileName);
+
+	files.sort().map(function (file, index) {
+		var structure = (index === files.length -1) ? '│   └── ' : '│   ├── ';
+	  console.log(structure + file);
+	});
+
+	console.log('└── ...');
+}
