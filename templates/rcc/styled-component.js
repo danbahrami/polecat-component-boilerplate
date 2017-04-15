610ca063340924bcfd65e19e8653347d12f86ae4
danbahrami <dan.bahrami@googlemail.com>
2017-04-15 12:05:10 +0100

    Add styled component temlates

---
+++ templates/rcc/styled-component.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+import styles from './$FILE_NAME$-style.scss';
+
+class $NAME$Component extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div/>
+  }
+}
+
+$NAME$Component.propTypes = {};
+
+export default $NAME$Component;
---
+++ templates/rfc/styled-component.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+import styles from './$FILE_NAME$-style.scss';
+
+const $NAME$Component = (props) => {
+  return (
+    <div/>
+  );
+};
+
+$NAME$Component.propTypes = {};
+
+export default $NAME$Component;
