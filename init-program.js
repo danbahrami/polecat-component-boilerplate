7780b0ada151e9ed7d0cd3687ee295b350171d06
danbahrami <dan.bahrami@googlemail.com>
2017-04-15 16:37:54 +0100

    Extract init function from root files

---
+++ init-program.js
@@ -0,0 +1,27 @@
+var program = require('commander');
+
+function init() {
+  program
+    .version('1.0.0')
+    .usage('[options] <path> <name>')
+    .action(function (path, name) {
+      if (typeof path !== 'string' || typeof name !== 'string') {
+        program.help();
+      }
+    })
+    .option('-c, --connected', 'Add a Redux connector')
+    .option('-s, --styled', 'Add an associated .scss file')
+    .parse(process.argv);
+
+  /*
+   * Catch case of user typing in the command
+   * with no trailing options or arguments.
+   */
+  if (process.argv.length <= 2) {
+    program.help();
+  }
+
+  return program;
+}
+
+module.exports = init;
--- rcc.js
+++ rcc.js
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
 var program = require('commander');
+var init = require('./init-program');
 var createBoilerplate = require('./create-boilerplate');
 
 /*
  * Initialise the program
  */
-program
-  .version('1.0.0')
-  .arguments('<path> <name>')
-  .option('-c, --connected', 'Add a Redux connector')
-  .option('-s, --styled', 'Add an associated .scss file')
-  .parse(process.argv);
+var program = init();
 
 /*
  * Run the program
